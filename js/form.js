e8e39ff67a489e65dbf814605b65bc63a10591c5
Lamorna Read <lamornaread@outlook.com>
2023-06-08 16:04:18 +0100

    Update form.js

--- js/form.js
+++ js/form.js
@@ -21,67 +21,147 @@ const countCharacters = () => {
 
 textArea.addEventListener("input", countCharacters);
 
-//form function
+// //form function
 
-function validateForm() {
-    const firstName = document.getElementById('fname').value;
-    const lastName = document.getElementById('lname').value;
-    const email = document.getElementById('email').value;
-    const subject = document.getElementById('subject').value;
-    const message = document.getElementById('message').value;
-    const emailFormat = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    const success = document.getElementById('success');
+// function validateForm() {
+//     const firstName = document.getElementById('fname').value;
+//     const lastName = document.getElementById('lname').value;
+//     const email = document.getElementById('email').value;
+//     const subject = document.getElementById('subject').value;
+//     const message = document.getElementById('message').value;
+//     const emailFormat = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+//     const success = document.getElementById('success');
 
-    //first name
-    if(firstName.length == 0) {
+//     //first name
+//     if(firstName.length == 0) {
+//         let fnameError = document.getElementById('firstname-error');
+//         fnameError.innerHTML = 'Please Enter First Name';
+//         return false;
+//     } 
+//     if(firstName.length > 0) {
+//         let fnameError = document.getElementById('firstname-error');
+//         fnameError.innerHTML = '';
+//     }
+//     //last name
+//     if(lastName.length == 0) {
+//         let lnameError = document.getElementById('lastname-error');
+//         lnameError.innerHTML = 'Please Enter Last Name';
+//         return false;
+//     }
+//     if(lastName.length > 0) {
+//         let fnameError = document.getElementById('lastname-error');
+//         fnameError.innerHTML = '';
+//     }
+//     //email
+//     if(!emailFormat.test(email) || email.length == 0) {
+//         let emailError = document.getElementById('email-error');
+//         emailError.innerHTML = 'Please Enter A Valid Email';
+//         return false;
+//     }
+//     if(emailFormat.test(email) || email.length > 0) {
+//         let emailError = document.getElementById('email-error');
+//         emailError.innerHTML = '';
+//     }
+//     //subject
+//     if(subject.length == 0) {
+//         let subjectError = document.getElementById('subject-error');
+//         subjectError.innerHTML = 'Please Enter A Subject';
+//         return false;
+//     }
+//     if(subject.length > 0) {
+//         let subjectError = document.getElementById('subject-error');
+//         subjectError.innerHTML = '';
+//     }
+//     //message
+//     if(message.length <= 140) {
+//         let messageError = document.getElementById('message-error');
+//         messageError.innerHTML = 'Your Message Is Too Short';
+//         return false;
+//     }
+//     if(message.length > 140) {
+//         let messageError = document.getElementById('message-error');
+//         messageError.innerHTML = '';
+//     }
+//     success.innerHTML = 'Submit Successful';
+//     success.style.color = '#24d36e';
+//     document.addEventListener('submit', function(event) {
+//         event.preventDefault();
+//         event.target.reset();
+//     });    
+//     return true;   
+// }
+
+
+const firstName = document.getElementById('fname').value;
+const lastName = document.getElementById('lname').value;
+const email = document.getElementById('email').value;
+const subject = document.getElementById('subject').value;
+const message = document.getElementById('message').value;
+const emailFormat = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const success = document.getElementById('success');
+
+
+function checkEmpty() {
+    if(firstName.length == 0 ) {
         let fnameError = document.getElementById('firstname-error');
         fnameError.innerHTML = 'Please Enter First Name';
-        return false;
+        return true;
     } 
+    if(lastName.length == 0) {
+        let lnameError = document.getElementById('lastname-error');
+        lnameError.innerHTML = 'Please Enter Last Name';
+        return true;
+    }
+    if(!emailFormat.test(email) || email.length == 0) {
+        let emailError = document.getElementById('email-error');
+        emailError.innerHTML = 'Please Enter A Valid Email';
+        return true;
+    }
+    if(subject.length == 0) {
+        let subjectError = document.getElementById('subject-error');
+        subjectError.innerHTML = 'Please Enter A Subject';
+        return true;
+    }
+    if(message.length <= 140) {
+        let messageError = document.getElementById('message-error');
+        messageError.innerHTML = 'Your Message Is Too Short';
+        return true;
+    } else {
+        return false
+    }
+}
+
+function checkFilled() {
     if(firstName.length > 0) {
         let fnameError = document.getElementById('firstname-error');
         fnameError.innerHTML = '';
     }
-    //last name
-    if(lastName.length == 0) {
-        let lnameError = document.getElementById('lastname-error');
-        lnameError.innerHTML = 'Please Enter Last Name';
-        return false;
-    }
+
     if(lastName.length > 0) {
         let fnameError = document.getElementById('lastname-error');
         fnameError.innerHTML = '';
-    }
-    //email
-    if(!emailFormat.test(email) || email.length == 0) {
-        let emailError = document.getElementById('email-error');
-        emailError.innerHTML = 'Please Enter A Valid Email';
-        return false;
     }
     if(emailFormat.test(email) || email.length > 0) {
         let emailError = document.getElementById('email-error');
         emailError.innerHTML = '';
     }
-    //subject
-    if(subject.length == 0) {
-        let subjectError = document.getElementById('subject-error');
-        subjectError.innerHTML = 'Please Enter A Subject';
-        return false;
-    }
     if(subject.length > 0) {
         let subjectError = document.getElementById('subject-error');
         subjectError.innerHTML = '';
     }
-    //message
-    if(message.length <= 140) {
-        let messageError = document.getElementById('message-error');
-        messageError.innerHTML = 'Your Message Is Too Short';
-        return false;
-    }
+
     if(message.length > 140) {
         let messageError = document.getElementById('message-error');
         messageError.innerHTML = '';
     }
+}
+
+
+function validateForm() {
+     if(checkEmpty() === true) {
+        return false;
+     }
+     checkFilled();
     success.innerHTML = 'Submit Successful';
     success.style.color = '#24d36e';
     document.addEventListener('submit', function(event) {
@@ -90,4 +170,3 @@ function validateForm() {
     });    
     return true;   
 }
-
